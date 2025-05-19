b7cb271915c31e3f3324e0f88d7f390ccc33c1ea
bhanuadusumalli26 <bhanu.adusumalli11@gmail.com>
2025-05-19 20:33:46 +0530

    Methods used to Rotate  an array by k times

---
+++ rotateArraybyK/Program2.java
@@ -0,0 +1,32 @@
+
+class Program2 {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7};
+        int k = 3;
+        int n = arr.length;
+        Rotateeletoright(arr, n, k);
+        System.out.print("After Rotating the k elements to right ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void Rotateeletoright(int[] arr, int n, int k) {
+        reverse(arr, 0, n - 1);
+        reverse(arr, 0, k - 1);
+        reverse(arr, k, n - 1);
+
+    }
+
+    public static void reverse(int[] arr, int start, int end) {
+        while (start <= end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
