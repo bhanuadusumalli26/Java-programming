385e2a668d796a7297b06755d773edb2ee26979f
bhanuadusumalli26 <bhanu.adusumalli11@gmail.com>
2026-01-30 21:10:30 +0530

    Binary Search on Rotated Array

---
+++ Binary_search/BS_1D_Array/BS_on_Rotated_Array/Program1.java
@@ -0,0 +1,34 @@
+
+class Program1 {
+
+    public static void main(String[] args) {
+        int[] arr = {4, 5, 6, 7, 0, 1, 2};
+        int target = 0;
+        System.out.println(search(arr, target));
+    }
+
+    public static int search(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[left] <= arr[mid]) {
+                if (arr[left] <= target && target <= arr[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+
+            } else {
+                if (arr[mid] <= target && target <= arr[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
